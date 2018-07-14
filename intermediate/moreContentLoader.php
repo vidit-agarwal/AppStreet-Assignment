ddb118aaa3b2b964269161594c8564edab901c06
Vidit <vidit-agarwal@users.noreply.github.com>
2018-07-14 17:19:27 +0530

    Uploading code for dynamic loading at listing page

--- intermediate/moreContentLoader.php
+++ intermediate/moreContentLoader.php
@@ -17,7 +17,7 @@
 		
 		foreach ($api_data as $api_data_name => $api_data_value) 
 		{
-  						$returnVal.= ' <li class="li-wrap">' ;
+  						$returnVal.= '<a href="item.php?_id='.$api_data_value['_id'].'"><li class="li-wrap">' ;
 	  					$returnVal.= '<div class="item-wrap">' ;
 						foreach ($api_data_value['images'] as $image_index => $img_value) {
 							$returnVal.= "<img src='".$img_value."' alt='product_image' class='rounded fix-images'>" ;
@@ -25,14 +25,15 @@
 						}
 		
 						$returnVal.= '<p class="h6 product_name">'.substr($api_data_value['name'], 0 , strrpos($api_data_value['name'], '(')).'</p>' ;
-						$returnVal.= '<p class="h6 product_price"> &#8377;'.$api_data_value['mark_price'].'</p>';	
+						$returnVal.= '<p class="h6 product_price"> &#8377;&nbsp;'.number_format($api_data_value['sale_price']).'</p>';	
 
 						$returnVal.= '</div>' ;
-						$returnVal.= '</li>' ;
+						$returnVal.= '</li></a>' ;
   						
 		}
 	}
-	echo $returnVal;		
+
+	echo $returnVal;	
 
 
 
